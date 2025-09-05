13948cbfe5a87fce0d33158e417cb2ad812d4073
InSyncWithFoo <insyncwithfoo@gmail.com>
2025-09-05 01:23:46 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## `ty-extensions`
+
+This repository contains a properly packaged fork
+of [the official `ty-extensions`][1], whose [PyPI project][2] is empty.
+
+
+  [1]: https://github.com/astral-sh/ruff/blob/HEAD/crates/ty_vendored/ty_extensions/ty_extensions.pyi
+  [2]: https://pypi.org/project/ty-extensions/
---
+++ update.py
@@ -0,0 +1,27 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "requests",
+# ]
+# ///
+
+from pathlib import Path
+from typing import Final
+
+import requests
+
+
+_SOURCE_FILE_URL: Final = \
+	"https://raw.githubusercontent.com/astral-sh/ruff/HEAD/crates/ty_vendored/ty_extensions/ty_extensions.pyi"
+
+_PROJECT_ROOT: Final = Path(__file__).parent
+_TY_EXTENSIONS_PYI: Final = _PROJECT_ROOT / 'src' / 'ty_extensions' / '__init__.pyi'
+
+
+def main() -> None:
+	new_file_content = requests.get(_SOURCE_FILE_URL).text
+	_TY_EXTENSIONS_PYI.write_text(new_file_content, encoding = 'utf-8')
+
+
+if __name__ == '__main__':
+	main()
